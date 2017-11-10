63e4b01b198d31d16a24febf8aa6a8514e4162a7
Jisoo Park <ptytoo3800@gmail.com>
2017-11-10 05:18:02 +0000

    change commit

--- app/controllers/lotto_controller.rb
+++ app/controllers/lotto_controller.rb
@@ -8,7 +8,7 @@ class LottoController < ApplicationController
   end
   
   def api
-    require 'httparty'
+    #require 'httparty'
     require 'json'
     
     url = "http://www.nlotto.co.kr/common.do?method=getLottoNumber&drwNo="
@@ -16,17 +16,33 @@ class LottoController < ApplicationController
     result = JSON.parse(response.body)
     
     @lotto = [*1..45].sample(6)
-     
+    bonus = result["bnusNo"]
+    
+    @arr = Array.new
+    
     result.each do |key, value|
-      arr << value if key.include? "drwtNo"
+      @arr << value if key.include? "drwtNo"
     end
     
-    jackpot = arr.sort
-    # api로 가져온 이번주 당첨 코드
-    arr = [jackpot]
+    @matching = @arr & @lotto
+    @result = ""
+    
     # lotto 추첨된 숫자와 arr안에 담긴 숫자를 비교
-    if @lotto == jackpot
-       
+    if @matching.count == 6    #case @matching count when ....
+      @result = "1등 15억"
+    elsif @matching.count == 5 && @lotto.include?(bonus)#우리 추첨번호에 bonus가 포함되어 있나?
+      @result = "2등 4천"
+    elsif @matching.count == 4
+      @result = "3등 120만원"
+    elsif @matching.count == 3
+      @result = "4등 50,000원"
+    elsif @matching.count == 2
+      @result = "3등 5,000원"  
+    else
+      @result = "꽝!"
+    end
+     
     # 6개 1등 / 5개 2등 / 4개 3등 / 1개 6등
      
   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'lotto/index'
 
   get 'lotto/show'
+  
+  get 'lotto/api'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
